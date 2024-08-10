1af6204629dbf1de8bf3e82da9508ab2cbebea7e
Guilherme Coelho <coelhoxz@gmail.com>
2024-08-10 16:56:30 -0300

    Importing only when necessary

--- examples/parse_solver.py
+++ examples/parse_solver.py
@@ -1,19 +1,18 @@
 import sys
 
-from pytosolver.solvers.highsapi import HighsApi
-from pytosolver.solvers.xpressapi import XpressApi
 from pytosolver.solvers.abstractsolverapi import AbstractSolverApi
 
 
 def get_solver_api() -> type[AbstractSolverApi]:
     solver_name = None if len(sys.argv) == 1 else str(sys.argv[1]).lower()
-    if solver_name is None:
-        return HighsApi
 
     match solver_name:
         case "xpress":
+            from pytosolver.solvers.xpressapi import XpressApi
             return XpressApi
         case "highs":
+            from pytosolver.solvers.highsapi import HighsApi
             return HighsApi
         case _:
+            from pytosolver.solvers.highsapi import HighsApi
             return HighsApi
